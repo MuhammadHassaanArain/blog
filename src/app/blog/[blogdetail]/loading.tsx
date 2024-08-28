aff45f55bfa13cf337b2f89e202cc8b92f3fc8db
Hassaan Arain <rafatnaz21@gmail.com>
2024-08-28 14:10:32 +0500

    First commit

---
+++ src/app/blog/[blogdetail]/loading.tsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+    // You can add any UI inside Loading, including a Skeleton.
+    return (
+        <div className="flex justify-center bg-orange-400">
+            <h1 className="font-bold">Loading...</h1>
+        </div>
+    )
+  };
